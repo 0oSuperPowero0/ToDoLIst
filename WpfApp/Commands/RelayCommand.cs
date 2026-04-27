f5e8ace66d97244bd4d9558af5be924d6d039a9f
Yeonjunam <gage2311@gmail.com>
2026-04-27 15:23:26 +0200

    commands hinzufügen

---
+++ WpfApp/Commands/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Windows.Input;
+
+namespace WpfApp.Commands;
+
+// Zwischenlink wpf button -> Methode
+public class RelayCommand : ICommand
+{
+	//field
+	private readonly Action _execute; 
+	private readonly Func<bool>? _canExecute; 
+
+	//Konstruktor
+	public RelayCommand (Action execute, Func<bool>? canExecute = null)
+	{
+		execute = _execute;
+		canExecute = _canExecute;
+	}
+
+	public bool CanExecute(object? parameter)// aktivieren oder deaktivieren den Button
+	{
+		return _canExecute == null || _canExecute();
+	}
+
+	public void Execute(object? parameter) //speichern die auszuführende Methode
+	{
+		_execute();
+	}
+
+	public event EventHandler? CanExectueChanged; // informieren Zustandsänderung
+
+	public void RaiseCanExectueChanged() //benachrichtigen WPF, CanExecute ernuet zu überprüfen
+	{
+		CanExectueChanged?.Invoke(this, EventArgs.Empty);
+	}
+}
---
+++ WpfApp/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models;
+
+public class TodoItem
+{
+	public string TodoText { get; set; } = "";
+	public bool IsDone { get; set; } = false;
+}
